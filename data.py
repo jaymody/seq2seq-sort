9487bfeb3acab49e3b60372b1a1a615a9c89ba87
jaymody <jaykmody@gmail.com>
2022-05-02 19:47:44 +0000

    Add replacement to CharSortedDataset

--- data.py
+++ data.py
@@ -23,14 +23,28 @@ class SimpleDataset(Dataset):
 
 
 class CharSortedDataset(Dataset):
-    def __init__(self, N, min_length=8, max_length=26):
+    def __init__(
+        self,
+        N,
+        min_length=1,
+        max_length=26,
+        population=string.ascii_lowercase,
+        replace=True,
+    ):
+        assert min_length >= 1
+        assert max_length >= min_length
+        if replace:
+            assert max_length <= len(population)
+
+        def random_sequence():
+            k = random.randint(min_length, max_length)
+            if replace:
+                return random.choices(population, k=k)
+            else:
+                return random.sample(population, k=k)
+
         self.N = N
-        self.srcs = [
-            random.sample(
-                string.ascii_lowercase, random.randint(min_length, max_length)
-            )
-            for _ in range(N)
-        ]
+        self.srcs = [random_sequence() for _ in range(N)]
         self.trgs = [sorted(src) for src in self.srcs]
         self.srcs = ["".join(src) for src in self.srcs]
         self.trgs = ["".join(trg) for trg in self.trgs]
